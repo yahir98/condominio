19519b09b227956769d13d90fece7e92bf9e94ba
yahir <yahirisaacurbina@gmail.com>
2019-10-11 00:56:53 -0600

    tercer commit
    metodo post

--- routes/api/condominios/index.js
+++ routes/api/condominios/index.js
@@ -1,8 +1,8 @@
 var express= require('express');
 var router = express.Router();
-var filemodel = require('../filemodel');
+var fileModel = require('../filemodel');
 
-var conCollection = filemodel.getCondominios();
+var conCollection = fileModel.getCondominios();
 
 router.get('/', function(req, res){
   res.json({
@@ -13,9 +13,45 @@ router.get('/', function(req, res){
 
 
 router.get('/all', function(req,res){
- conCollection=filemodel.getCondominios();
+ conCollection=fileModel.getCondominios();
  res.json(conCollection);
 
 }) //get all
 
 module.exports = router;
+
+
+
+//metodo post//********************************************* */
+router.post('/new', function(req, res){
+    conCollection = fileModel.getCondominios();
+    var newCondominios = Object.assign(
+       {},
+       req.body,
+       {
+           "nombre": req.body.nombre,
+           "apartament": parseFloat(req.body.apartament),
+           "cuotaMensual": parseFloat(req.body.cuota)
+       }
+    );
+    var condominioExists = conCollection.find(
+      function(o, i){
+        return o.codigo === newCondominios.codigo;
+      }
+    )
+    if( ! condominioExists ){
+      conCollection.push(newCondominios);
+      fileModel.setCondominios(
+         conCollection,
+         function(err, savedSuccesfully){
+           if(err){
+             res.status(400).json({ "error": "No se pudo ingresar objeto" });
+           } else {
+             res.json(newCondominios);  // req.body ===  $_POST[]
+           }
+         }
+       );
+    } else {
+      res.status(400).json({"error":"No se pudo ingresar objeto"});
+    }
+ }); // post /new
